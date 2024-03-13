b9ecd83b9ca4ae2c98b58937637fee1c25476d11
Your Name <you@example.com>
2024-03-13 07:42:05 +0530

    changed the toast

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -21,7 +21,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           rtl={false}
           pauseOnFocusLoss
           draggable
-          pauseOnHover
+          // pauseOnHover
           theme="colored"
         />
       </ContextProvider>
